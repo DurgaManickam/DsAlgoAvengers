4137b0be1cde5b18a07b3e68028dd20e32327af2
VD136 <150267565+VD136@users.noreply.github.com>
2023-12-08 10:36:01 -0500

    Add files via upload
    SD files added

---
+++ DataStructures_SD.java
@@ -0,0 +1,43 @@
+package stepdefinition;
+
+import org.openqa.selenium.WebDriver;
+
+import driverFactory.DriverManager;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import webPages.DataStructurePage;
+
+
+public class DataStructures_SD {
+	private WebDriver driver= DriverManager.getDriver();
+	DataStructurePage ds=new DataStructurePage(driver);
+	
+	@Given("Login to DsAlsgo website with valid username {string} and password {string} and navigate to DataStructures page.")
+	public void login_and_navigate_to_data_structures_page(String username, String password) {
+		driver.get("https://dsportalapp.herokuapp.com/login");
+	    ds.login(username,password);
+	    System.out.println("Current URL of the page after selecting the dropdown option : " + driver.getCurrentUrl());
+	}
+
+	@When("user in DS page clicks on {string} with the corresponding {string}")
+	public void user_ds_page_clicks_link(String pagename, String url) {
+		ds.click_Page_DataStructure(pagename, url);
+	   
+	}
+
+	@Then("user must be directed to {string} of DS page")
+	public void user_navigate_to_ds_page(String url) {
+	    ds.ValidatePage_DataStructure(url);
+	}
+
+	@Then("click on Tryhere button in DS page and click the run button.")
+	public void click_tryhere_button_in_ds_page_and_click_the_run_button() {
+	    ds.click_Tryhere();
+	}
+
+	@Given("User navigates to DataStructures Time complexity page.")
+	public void user_navigates_to_data_structures_representations() {
+	   driver.get("https://dsportalapp.herokuapp.com/data-structures-introduction/time-complexity/");
+	}
+}
---
+++ Signout_SD.java
@@ -0,0 +1,34 @@
+package stepdefinition;
+
+import org.openqa.selenium.WebDriver;
+
+import driverFactory.DriverManager;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import webPages.Signout;
+
+
+public class Signout_SD {
+
+	private WebDriver driver= DriverManager.getDriver();
+	Signout sign=new Signout(driver);
+	
+	@Given("Login to DsAlsgo website with valid username {string} and password {string} for validating signout button.")
+	public void login_and_navigate_to_data_structures_page(String username, String password) {
+		driver.get("https://dsportalapp.herokuapp.com/login");
+		sign.login(username,password);
+	    System.out.println("Current URL of the page after selecting the dropdown option : " + driver.getCurrentUrl());
+	}
+	
+	 @When("user clicks on signout button.")
+	 public void clickon_signout() {
+		 sign.signout();
+	 }
+	 
+	    @Then("user must be logged out and navigate to Home page{string}.")
+	    public void validateLogout(String url) {
+	    	sign.validatesignoutuser(url);
+	    }
+
+}
